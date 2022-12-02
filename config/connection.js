e3c8ccbc3b6f4af51816ae8b9cb5268519dda6d2
sufiyanbmk <sufiyanbmk@gmail.com>
2022-12-02 19:39:54 +0530

    phontos

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
     db:null
 }
 module.exports.connect = function(done){
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb+srv://muhammadsufiyan:yQXnFH3KbpgiXPmU@cluster0.ypqligh.mongodb.net/?retryWrites=true&w=majority'
     const dbname = 'project-ecommerce'
 
     mongoclient.connect(url,(err,data)=>{
